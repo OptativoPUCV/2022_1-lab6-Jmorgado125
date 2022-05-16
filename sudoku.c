3dcff3db9cbdf32ba406d3bd104593f206cba6fb
Your Name <you@example.com>
2022-05-16 19:41:46 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,24 +42,75 @@ void print_node(Node* n){
     }
     printf("\n");
 }
+//recorrer submatriz parte del 0 hasta 8
+/*int recorrerSubmatriz(Node * nodo){
+  int k=4,p;
+  int contS=0;
+  for(p=0;p<9;p++){
+    int i=3*(k/3) + (p/3) ;
+    int j=3*(k%3) + (p%3) ;
+    printf("%d ",nodo->sudo[i][j]);
+    if(p%3 == 2) printf("\n");
+   
+  return contS;
+}*/
+//crear arreglo(arreglo de 10 elem valor 0) donde las posiciones corresponden al valor aux y si se supera es mas de 1
 
-int is_valid(Node* n){
-
-    return 1;
+int is_valid(Node* nodo){
+  /*int i=0,j=0,aux=1;
+  int arregloPos[10];
+ 
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
+      if(nodo->sudo[i][j])
+     
+       
+    }
+  }*/
+  return 1;
 }
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    return list;
+  List* list=createList();
+  int i,j,RA=1;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
+      if (n->sudo[i][j] == 0){
+        while(RA < 10){
+          Node * adj_n = (Node*) malloc(sizeof(Node));
+          adj_n=copy(n);
+          adj_n->sudo[i][j] = RA;
+          //if (is_valid == 1){}
+          push(list,adj_n);
+          RA++;
+        }
+      }
+    }
+  }
+  return list;
 }
 
 
 int is_final(Node* n){
-    return 0;
+  int i,j;
+  //if(is_valid(n) == 0)return0;
+  for(i=0;i<9;i++){
+    for(j=0;j<9;j++){
+      if(n->sudo[i][j] == 0){}
+      return 0;  
+    }
+  }
+    return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
+  /*Stack * P=createStack();
+  push(p,initial);
+  while(Pila->size != 0){
+   
+  }
+  */
   return NULL;
 }
 
